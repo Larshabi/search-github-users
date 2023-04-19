a207aec187a4c699799993c55efdb05ecfa5350f
Olalekan Lasabi <lekanlasabi@gmail.com>
2023-04-19 17:40:08 +0100

    search-github-users

---
+++ src/components/Charts/Doughnut2d.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+const ChartComponent = ({data})=>{
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      chart: {
+        caption: 'Stars Per Languages',
+        theme:'fusion',
+        decimals: 0,
+        doughnutRadius: '45%',
+        showPercentValues:0,
+        theme:'candy'
+        // paletteColors:'#f0db4f'
+      },
+      data 
+    }
+  };
+  
+  return (<ReactFC {...chartConfigs} />)
+}
+
+
+export default ChartComponent;
